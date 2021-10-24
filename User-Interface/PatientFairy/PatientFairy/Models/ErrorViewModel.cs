63f8f91399bac66a7ab249a04f2dcc563ecc6803
ashkan <ashkan_pirmani@outlook.com>
2021-10-24 10:03:48 +0200

    User Interface

---
+++ User-Interface/PatientFairy/PatientFairy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PatientFairy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
